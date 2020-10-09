31c7465c8d83f89b5c1683c115ce7d65d4536549
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2020-10-09 15:08:12 -0600

    Initial commit

---
+++ src/Modal.js
@@ -0,0 +1,62 @@
+import React, {useState, useRef} from "react";
+import styled from 'styled-components';
+
+const OpenModalButton = styled.button`
+
+`;
+
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1040;
+  background-color: #000;
+  opacity: 0.5;
+`;
+
+const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpen = () => {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <>
+      {/* Trigger the modal with a button */}
+      <OpenModalButton type="button" onClick={toggleIsOpen}>Open Modal</OpenModalButton>
+
+      {isOpen &&
+        <>
+          {/* Modal */}
+          <div id={`modal-1`} className="modal fade" role="dialog" >
+            <div className="modal-dialog">
+
+              {/* Modal content */}
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h4 className="modal-title">Edit Todo</h4>
+                  <button type="button" className="close" data-dismiss="modal" >&times;</button>
+                </div>
+                <div className="modal-body">
+                  <input type="text" className="form-control"  />
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-warning" data-dismiss="modal" >Edit</button>
+                  <button type="button" className="btn btn-default" data-dismiss="modal" >Close</button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <Backdrop />
+        </>
+      }
+    </>
+    
+  );
+}
+
+export default Modal;
